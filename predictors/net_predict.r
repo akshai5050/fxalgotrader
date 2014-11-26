b134cc4cf89c2e56162547764c91eb3fd257711b
Akshai Shah <akshai5050@gmail.com>
2014-11-26 16:40:13 +0000

    Commit

---
+++ predictors/net_predict.r
@@ -0,0 +1,9 @@
+load("/Users/shaha1/nnet_model.RData")
+
+pred <- function(a) {
+   df <- as.data.frame(rbind(c(a)))
+   print(df)
+   names (df) <- c("x1", "x2", "x3", "x4", "x5")
+   res <- predict(model, df)[1]
+   res
+}
